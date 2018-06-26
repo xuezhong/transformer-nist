fe6ae66bbc98b326c07a358cb159a438af85cefd
xuezhong <xuezhongq@gmail.com>
2018-06-26 04:45:11 +0000

    add transformer_m_cpu.sh

---
+++ cloud/transformer_m_cpu.sh
@@ -0,0 +1,16 @@
+paddlecloud job train \
+--job-version custom-fluid \
+--cluster-name yq01-jpaas-paddle01-cpu \
+--k8s-priority high \
+--k8s-wall-time 10:00:00 \
+--k8s-memory 100Gi \
+--k8s-ps-memory 20Gi \
+--job-name m-cpu-transformer \
+--start-cmd "python -u thirdparty/model/transformer_cloud/train.py --src_vocab_fpath ./thirdparty/nist06n/cn_30001.dict --trg_vocab_fpath ./thirdparty/nist06n/en_30001.dict --train_file_pattern './train/part-*' --batch_size 1024 --use_token_batch True  --device CPU --special_token '_GO' '_EOS' '_UNK'" \
+--job-conf transformer/common.py \
+--files transformer/common.py \
+--k8s-not-local  \
+--k8s-trainers 4 \
+--k8s-ps-num 2 \
+--k8s-ps-cores 1 \
+--image-addr registry.baidu.com/qiaolongfei/paddlepaddle-cpu:ctr
